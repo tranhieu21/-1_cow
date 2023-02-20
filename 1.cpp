015c872e2cbeac62cb25afb12e7213d9e795dcbf
tranhieu21 <114563520+tranhieu21@users.noreply.github.com>
2023-02-20 12:20:48 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  newcow
+//
+//  Created by Trần Trung Hiếu on 06/02/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
